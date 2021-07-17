2916ca70b36c42761f27c153e998d8030b15c7c3
weat0212 <weat0212@gmail.com>
2021-07-17 16:37:35 +0800

    first commit

---
+++ src/test/java/com/example/microserviceusermanagement/MicroserviceUserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microserviceusermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceUserManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
